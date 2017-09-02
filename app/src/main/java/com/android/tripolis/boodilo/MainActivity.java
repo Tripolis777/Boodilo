fd842ab9512c5355826338f3089e324fd4b43240
v.karyagin <tripolis777@gmail.com>
2017-09-02 10:41:46 +0300

    save commit

--- app/src/main/java/com/android/tripolis/boodilo/MainActivity.java
+++ app/src/main/java/com/android/tripolis/boodilo/MainActivity.java
@@ -1,13 +1,14 @@
 package com.android.tripolis.boodilo;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import com.android.tripolis.boodilo.Adapters.AlarmListAdapter;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private ListView alarmsList;
 
+    private FrameLayout fragmentPlaceholder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +33,14 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //fragmentPlaceholder = (FrameLayout) findViewById(R.id.fragment_placeholder);
+
         //alarmsList = (ListView) findViewById(R.id.alarmsList);
         //alarmsList.setAdapter(new AlarmListAdapter(getApplicationContext(), new ArrayList<Alarm>()));
-        FragmentManager fm = getFragmentManager();
+        FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         AlarmsListFragment fragment = new AlarmsListFragment();
-        ft.add(R.id.fragment, fragment, fragment.getClass().toString());
-        ft.commit();
+        ft.add(R.id.fragment_placeholder, fragment, fragment.getClass().toString()).commit();
     }
 
     @Override
@@ -52,14 +55,14 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        FragmentManager fm = getFragmentManager();
+        FragmentManager fm = getSupportFragmentManager();
 
 
         switch (item.getItemId()) {
-            case R.id.action_favorite:
+            case R.id.action_add_alarm:
                 FragmentTransaction fragmentTransaction = fm.beginTransaction();
                 AlarmAddFragment fragment = new AlarmAddFragment();
-                fragmentTransaction.add(R.id.fragment, fragment, "AlarmAddFragment");
+                fragmentTransaction.add(R.id.fragment_placeholder, fragment);
                 fragmentTransaction.commit();
                 return true;
             case R.id.action_settings:
